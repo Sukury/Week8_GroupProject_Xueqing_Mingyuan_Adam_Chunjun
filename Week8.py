f9dc543005c54ff946c5d19d7ffc108ff66c33b4
Sukury <xh2522@nyu.edu>
2023-10-25 18:43:07 -0400

    data

--- Week8.py
+++ Week8.py
@@ -1,3 +1,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+#open data
+Course_Section_info = pd.read_csv("data/Course section info.csv")
+student_courseInfo=pd.read_csv("data/Student - course section info.csv")
+student_careerInfo=pd.read_csv("data/Student career info.csv")
+student_info=pd.read_csv("data/Student info.csv")
+student_termInfo=pd.read_csv("data/Student term info.csv")
